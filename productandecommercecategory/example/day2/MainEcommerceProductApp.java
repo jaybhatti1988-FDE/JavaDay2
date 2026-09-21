cca87526408ee80f313fe61940330c290b581001
jaybhatti1988-FDE <jaybhatti1988@gmail.com>
2026-09-21 10:49:59 +0530

    Product and E-Commerce Category
    Create a base class Product with product ID, product name, and price. Create a subclass  ElectronicProduct that adds warranty period and brand name. Display all product  information.

---
+++ productandecommercecategory/example/day2/MainEcommerceProductApp.java
@@ -0,0 +1,37 @@
+package com.inheritance.productandecommercecategory.example.day2;
+
+import java.util.Scanner;
+
+public class MainEcommerceProductApp {
+
+	public static void main(String[] args) {
+
+		Scanner scanProduct=new Scanner(System.in);
+		
+		System.out.print("Enter Product Id ");
+		String productId=scanProduct.nextLine();
+
+		System.out.print("Enter Product Name :- ");
+		String prdName=scanProduct.nextLine();
+				
+		System.out.print("Enter Product Price :- ");
+		float prdPrice=scanProduct.nextFloat();
+		scanProduct.nextLine();
+		
+		System.out.print("Enter Product Quantity :- ");
+		int prdqty=scanProduct.nextInt();
+		scanProduct.nextLine();
+				
+		System.out.print("Enter Product Brand Name :- ");
+		String prdBrndName=scanProduct.nextLine();
+		
+		System.out.print("Enter Product Warranty Periods :- ");
+		int prdwrtyPriods=scanProduct.nextInt();
+		scanProduct.nextLine();
+		
+		ElectronicProduct eprodct=new ElectronicProduct(productId, prdName, prdPrice,prdqty,prdBrndName,prdwrtyPriods);
+		eprodct.EcommerceProductDetails();
+		scanProduct.close();
+	}
+
+}
